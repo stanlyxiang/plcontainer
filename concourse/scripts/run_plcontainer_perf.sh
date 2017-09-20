622f43dd3e2c1ccfa2ee31d76a16704178e18959
stanlyxiang <stanly.sxiang@gmail.com>
2017-09-20 16:58:58 +0800

    build R image for plcontainer

--- concourse/scripts/run_plcontainer_perf.sh
+++ concourse/scripts/run_plcontainer_perf.sh
@@ -12,19 +12,21 @@ source /usr/local/greenplum-db-devel/greenplum_path.sh; \
 gppkg -i /tmp/plcontainer_gpdb_build/plcontainer-concourse-centos*.gppkg; \
 \""
 
-scp -r plcontainer_client_docker_image/plcontainer-devel-images.tar.gz mdw:/usr/local/greenplum-db-devel/share/postgresql/plcontainer/
+scp -r plcontainer_pyclient_docker_image/plcontainer-devel-images.tar.gz mdw:/usr/local/greenplum-db-devel/share/postgresql/plcontainer/
+scp -r plcontainer_rclient_docker_image/plcontainer-devel-images.tar.gz mdw:/usr/local/greenplum-db-devel/share/postgresql/plcontainer/
 
 ssh mdw "bash -c \" \
 set -eox pipefail; \
 export MASTER_DATA_DIRECTORY=/data/gpdata/master/gpseg-1; \
 source /usr/local/greenplum-db-devel/greenplum_path.sh; \
-plcontainer install -n plc_python_shared -i /usr/local/greenplum-db-devel/share/postgresql/plcontainer/plcontainer-devel-images.tar.gz -c pivotaldata/plcontainer_python_shared:devel; \
+plcontainer install -n plc_python_shared -i /usr/local/greenplum-db-devel/share/postgresql/plcontainer/plcontainer-python-devel-images.tar.gz -c pivotaldata/plcontainer_python_shared:devel; \
+plcontainer install -n plc_r_shared -i /usr/local/greenplum-db-devel/share/postgresql/plcontainer/plcontainer-r-devel-images.tar.gz -c pivotaldata/plcontainer_r_shared:devel; \
 gpstop -arf
 psql -d postgres -f /usr/local/greenplum-db-devel/share/postgresql/plcontainer/plcontainer_install.sql; \
-psql -d postgres -f plcontainer_src/concourse/scripts/function_setup.sql; \
-psql -d postgres -f plcontainer_src/concourse/scripts/perf_prepare1.sql; \
-nohup psql -d postgres -f plcontainer_src/concourse/scripts/perf_pl1.sql > perf_pl1 2>&1; \
-nohup psql -d postgres -f plcontainer_src/concourse/scripts/perf_py1.sql > perf_py1 2>&1; \
+psql -d postgres -f plcontainer_src/tests/perfsql/function_setup.sql; \
+psql -d postgres -f plcontainer_src/tests/perfsql/perf_prepare1.sql; \
+nohup psql -d postgres -f plcontainer_src/tests/perfsql/perf_pl1.sql > perf_pl1 2>&1; \
+nohup psql -d postgres -f plcontainer_src/tests/perfsql/perf_py1.sql > perf_py1 2>&1; \
 \""
 
 scp mdw:~/perf_pl1 plcontainer_perf_result/perf_pl1
--- concourse/scripts/run_plcontainer_tests.sh
+++ concourse/scripts/run_plcontainer_tests.sh
@@ -11,13 +11,15 @@ source /usr/local/greenplum-db-devel/greenplum_path.sh; \
 gppkg -i /tmp/plcontainer_gpdb_build/plcontainer-concourse-centos*.gppkg; \
 \""
 
-scp -r plcontainer_client_docker_image/plcontainer-devel-images.tar.gz mdw:/usr/local/greenplum-db-devel/share/postgresql/plcontainer/
+scp -r plcontainer_pyclient_docker_image/plcontainer-*.tar.gz mdw:/usr/local/greenplum-db-devel/share/postgresql/plcontainer/
+scp -r plcontainer_rclient_docker_image/plcontainer-*.tar.gz mdw:/usr/local/greenplum-db-devel/share/postgresql/plcontainer/
 
 ssh mdw "bash -c \" \
 set -eox pipefail; \
 export MASTER_DATA_DIRECTORY=/data/gpdata/master/gpseg-1; \
 source /usr/local/greenplum-db-devel/greenplum_path.sh; \
-plcontainer install -n plc_python_shared -i /usr/local/greenplum-db-devel/share/postgresql/plcontainer/plcontainer-devel-images.tar.gz -c pivotaldata/plcontainer_python_shared:devel; \
+plcontainer install -n plc_python_shared -i /usr/local/greenplum-db-devel/share/postgresql/plcontainer/plcontainer-python-devel-images.tar.gz -c pivotaldata/plcontainer_python_shared:devel; \
+plcontainer install -n plc_r_shared -i /usr/local/greenplum-db-devel/share/postgresql/plcontainer/plcontainer-r-devel-images.tar.gz -c pivotaldata/plcontainer_r_shared:devel; \
 gpstop -arf
 psql -d postgres -f /usr/local/greenplum-db-devel/share/postgresql/plcontainer/plcontainer_install.sql; \
 pushd plcontainer_src/tests; \
